c31d333013a9a38933ed86762baba2e083e1232b
Martin B <mbompr@gmail.com>
2018-02-21 14:55:47 +0100

    Add Hawkes earthquake aftershocks study example
    We need to install basemap to make it work

--- tools/travis/osx_install.sh
+++ tools/travis/osx_install.sh
@@ -31,5 +31,7 @@ python -m pip install -r requirements.txt
 python -m pip install sphinx pillow
 python -m pip install cpplint
 python -m pip install tensorflow
+brew install geos
+python -m pip install  https://github.com/matplotlib/basemap/archive/v1.1.0.tar.gz
 pyenv rehash
 
